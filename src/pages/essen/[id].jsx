08dabcaaa91a801a9b808c8451fb0ba93466af1e
danimisme <msubhanr53@gmail.com>
2024-03-24 21:18:57 +0700

    create button to back, edit and delete

--- src/pages/essen/[id].jsx
+++ src/pages/essen/[id].jsx
@@ -1,5 +1,6 @@
 import DetailFoodCard from "@/components/DetailFoodCard";
 import axios from "axios";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(context) {
   const res = await axios.get(
@@ -10,10 +11,25 @@ export async function getServerSideProps(context) {
 }
 
 export default function DetailFoodPage({ food }) {
+  const { push } = useRouter();
   return (
     <div>
       <h1 className=" text-2xl font-bold text-center mt-3">Detail Food</h1>
       <DetailFoodCard food={food} />
+      <div className="flex justify-center gap-5 mt-5">
+        <button
+          className="bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => push("/essen")}
+        >
+          Back
+        </button>
+        <button className="bg-green-700 text-white font-bold py-2 px-4 rounded">
+          Edit
+        </button>
+        <button className="bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
