785b579d42ff7e4dbe34d67e23348bfdc917e608
danimisme <msubhanr53@gmail.com>
2024-03-24 23:51:39 +0700

    add style for FormFood components

--- src/components/FormFood.jsx
+++ src/components/FormFood.jsx
@@ -1,14 +1,23 @@
 import InputForm from "./InputForm";
+import styles from "@/styles/FormFood.module.css";
+import { useState } from "react";
 
 export default function FormFood({ food }) {
-  console.log(food);
+  const [show, setShow] = useState(true);
   const handleChange = (event) => {
     console.log(event.target.value);
   };
   return (
-    <div>
-      <h1 className=" text-2xl font-bold text-center mt-3">Form Food</h1>
-      <form className="max-w-sm mx-auto">
+    <div
+      className={`${styles.modal_form_food} ${
+        show ? `${styles.show}` : `${styles.hide}`
+      }`}
+    >
+      <form
+        className={`${styles.form_food} max-w-sm gap-2 p-5 w-3/4 mx-auto rounded-xl `}
+      >
+        <h1 className=" text-2xl font-bold text-center mt-3">Form Food</h1>
+
         <InputForm
           label="Name"
           type="text"
@@ -43,6 +52,12 @@ export default function FormFood({ food }) {
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         >
           Submit
+        </button>
+        <button
+          className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
+          onClick={() => setShow(false)}
+        >
+          Cancel
         </button>
       </form>
     </div>
