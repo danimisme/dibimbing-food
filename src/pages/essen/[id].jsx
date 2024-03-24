fd67fcc86ab98a633824696232a7dcec06ab81de
danimisme <msubhanr53@gmail.com>
2024-03-24 20:35:10 +0700

    Dynamic data server side data fetching

--- src/pages/essen/[id].jsx
+++ src/pages/essen/[id].jsx
@@ -1,4 +1,15 @@
-export default function DetailFoodPage() {
+import axios from "axios";
+
+export async function getServerSideProps(context) {
+  const res = await axios.get(
+    `https://api-bootcamp.do.dibimbing.id/api/v1/foods/${context.params.id}`,
+    { headers: { apiKey: "w05KkI9AWhKxzvPFtXotUva-" } }
+  );
+  return { props: { food: res?.data?.data } };
+}
+
+export default function DetailFoodPage(food) {
+  console.log(food);
   return (
     <div>
       <h1 className=" text-2xl font-bold text-center">Detail Food</h1>
