306dc694888b1eaa60e9ba16495d406e8f536a40
danimisme <msubhanr53@gmail.com>
2024-03-26 23:34:10 +0700

    refactoring post function using async await funtion

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -6,22 +6,24 @@ export default function usePost() {
   const dispatch = useDispatch();
   const router = useRouter();
   const post = async (url, body) => {
-    axios
-      .post(`https://api-bootcamp.do.dibimbing.id/api/v1/${url}`, body, {
-        headers: {
-          apiKey: "w05KkI9AWhKxzvPFtXotUva-",
-          "Content-Type": "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pZnRhaGZhcmhhbkBnbWFpbC5jb20iLCJ1c2VySWQiOiJjYTIzZDdjYy02Njk1LTQzNGItODE2Yy03ZTlhNWMwNGMxNjQiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NjE4NzUzMjF9.wV2OECzC25qNujtyb9YHyzYIbYEV-wud3TQsYv7oB4Q",
-        },
-      })
-      .then(() => {
-        dispatch(hide());
-        router.reload();
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+    try {
+      await axios.post(
+        `https://api-bootcamp.do.dibimbing.id/api/v1/${url}`,
+        body,
+        {
+          headers: {
+            apiKey: "w05KkI9AWhKxzvPFtXotUva-",
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pZnRhaGZhcmhhbkBnbWFpbC5jb20iLCJ1c2VySWQiOiJjYTIzZDdjYy02Njk1LTQzNGItODE2Yy03ZTlhNWMwNGMxNjQiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NjE4NzUzMjF9.wV2OECzC25qNujtyb9YHyzYIbYEV-wud3TQsYv7oB4Q",
+          },
+        }
+      );
+      dispatch(hide());
+      router.reload();
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
 
   return { post };
