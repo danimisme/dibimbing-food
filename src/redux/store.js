178e3283abd5356597d80b6c77715e9acdccd1b1
danimisme <msubhanr53@gmail.com>
2024-03-26 22:11:48 +0700

    create modalDeleteReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
+import modalDeleteShowReducer from "./reducers/ModalDeleteReducer";
+
 const { combineReducers, createStore } = require("redux");
 const { default: modalShowReducer } = require("./reducers/ModalShowReducers");
 
 const rootReducer = combineReducers({
   modalShow: modalShowReducer,
+  modalDelete: modalDeleteShowReducer,
 });
 
 const store = createStore(rootReducer);
